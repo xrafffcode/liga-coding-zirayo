851e77f907272945d500b0c94dd2df1fd5f0d8d7
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-08-21 02:03:01 +0700

    Change style✨

--- assets/js/script-quiz.js
+++ assets/js/script-quiz.js
@@ -1,6 +1,171 @@
-"use strict"; const quizData = [{ question: "Tanggal berapa diperingatinya hari kemerdekaan Bangsa Indonesia?", a: "8 Maret 1942", b: "29 April 1945", c: "6 Agustus 1945", d: "17 Agustus 1945", e: "10 November 1948", correct: "d" }, { question: "Tanggal berapa Gubernur Jenderal Tjarda Van Starkenborgh Stachouwer ditawan oleh Jepang?", a: "8 Maret 1942", b: "10 November 1948", c: "6 Agustus 1945", d: "29 April 1945", e: "17 Agustus 1945", correct: "a" }, { question: "Kota di Jepang yang dijatuhi bom pertama kali oleh Amerika Serikat adalah?", a: "Hiroshima", b: "Nagasaki", c: "Kyoto", d: "Tokyo", e: "Osaka", correct: "a" }, { question: "Setelah BPUPKI dibubarkan, lalu dibentuk organisasi baru bernama?", a: "PETA", b: "Boedi Oetomo", c: "Sarekat Islam", d: "Indische Partij", e: "PPKI", correct: "e" }, { question: "Kota di Jepang yang dijatuhi bom kedua oleh Amerika Serikat adalah?", a: "Hiroshima", b: "Nagasaki", c: "Kyoto", d: "Tokyo", e: "Osaka", correct: "b" }, { question: "Naskah Proklamasi disusun di rumah?", a: "Hitoshi Imamura", b: "Laksamana Maeda", c: "Marsekal Terauchi", d: "Uzumaki Naruto", e: "Uchiha Itachi", correct: "b" }, { question: "Penjajahan bangsa Indonesia oleh Belanda terjadi kurang lebih selama?", a: "350 tahun", b: "250 tahun", c: "150 tahun", d: "50 tahun", e: "1 tahun", correct: "a" }, { question: "Tokoh yang mengusulkan agar naskah proklamasi ditandangani oleh Soekarno adalah?", a: "Mohammad Ibnu Sayuri", b: "Mohammad Yamin", c: "Chaerul Saleh", d: "Soekarni", e: "Johannes Latuharhary", correct: "d" }, { question: "Pencipta lagu Indonesia Raya adalah?", a: "Agus salim", b: "Sutan Sjahrir", c: "Wage Rudolf Supratman", d: "Soeharto", e: "Moh Hatta", correct: "c" }, { question: "Siapa yang mengetik naskah proklamasi?", a: "Dewi Sartika", b: "Cut Nyak Dien", c: "R.A. Kartini", d: "Fatmawati", e: "Sayuti Melik", correct: "e" },], quizContainer = document.querySelector(".app"), answerElements = document.querySelectorAll(".answer"), questionElement = document.querySelector(".question"), aAnswer = document.getElementById("a--answer"), bAnswer = document.getElementById("b--answer"), cAnswer = document.getElementById("c--answer"), dAnswer = document.getElementById("d--answer"), eAnswer = document.getElementById("e--answer"), submit = document.querySelector(".btn"), AUDIOC = { CLICK: new Audio("/assets/hooh.mp3") }, AUDIOW = { CLICK: new Audio("/assets/ente.mp3") }; let currentQuiz = 0, score = 0; const resetAnswers = function () { answerElements.forEach(a => a.checked = !1) }, loadQuiz = function () { resetAnswers(); let a = quizData[currentQuiz]; questionElement.textContent = a.question, aAnswer.textContent = a.a, bAnswer.textContent = a.b, cAnswer.textContent = a.c, dAnswer.textContent = a.d, eAnswer.textContent = a.e }; loadQuiz(); const getSelected = function () { let a; return answerElements.forEach(b => { b.checked && (a = b.id) }), a }; submit.addEventListener("click", function () {
-  let a = getSelected(); a && (a === quizData[currentQuiz].correct ? (AUDIOC.CLICK.play(), score++) : AUDIOW.CLICK.play(), ++currentQuiz < quizData.length ? loadQuiz() : quizContainer.innerHTML = `<h2 class="question">Kamu menjawab ${score}/${quizData.length} pertanyaan dengan benar</h2>
+"use strict";
+
+const quizData = [
+  {
+    question:
+      "Tanggal berapa diperingatinya hari kemerdekaan Bangsa Indonesia?",
+    a: "8 Maret 1942",
+    b: "29 April 1945",
+    c: "6 Agustus 1945",
+    d: "17 Agustus 1945",
+    e: "10 November 1948",
+    correct: "d",
+  },
+  {
+    question:
+      "Tanggal berapa Gubernur Jenderal Tjarda Van Starkenborgh Stachouwer ditawan oleh Jepang?",
+    a: "8 Maret 1942",
+    b: "10 November 1948",
+    c: "6 Agustus 1945",
+    d: "29 April 1945",
+    e: "17 Agustus 1945",
+    correct: "a",
+  },
+  {
+    question:
+      "Kota di Jepang yang dijatuhi bom pertama kali oleh Amerika Serikat adalah?",
+    a: "Hiroshima",
+    b: "Nagasaki",
+    c: "Kyoto",
+    d: "Tokyo",
+    e: "Osaka",
+    correct: "a",
+  },
+  {
+    question:
+      "Setelah BPUPKI dibubarkan, lalu dibentuk organisasi baru bernama?",
+    a: "PETA",
+    b: "Boedi Oetomo",
+    c: "Sarekat Islam",
+    d: "Indische Partij",
+    e: "PPKI",
+    correct: "e",
+  },
+  {
+    question:
+      "Kota di Jepang yang dijatuhi bom kedua oleh Amerika Serikat adalah?",
+    a: "Hiroshima",
+    b: "Nagasaki",
+    c: "Kyoto",
+    d: "Tokyo",
+    e: "Osaka",
+    correct: "b",
+  },
+  {
+    question: "Naskah Proklamasi disusun di rumah?",
+    a: "Hitoshi Imamura",
+    b: "Laksamana Maeda",
+    c: "Marsekal Terauchi",
+    d: "Uzumaki Naruto",
+    e: "Uchiha Itachi",
+    correct: "b",
+  },
+  {
+    question:
+      "Penjajahan bangsa Indonesia oleh Belanda terjadi kurang lebih selama?",
+    a: "350 tahun",
+    b: "250 tahun",
+    c: "150 tahun",
+    d: "50 tahun",
+    e: "1 tahun",
+    correct: "a",
+  },
+  {
+    question:
+      "Tokoh yang mengusulkan agar naskah proklamasi ditandangani oleh Soekarno adalah?",
+    a: "Mohammad Ibnu Sayuri",
+    b: "Mohammad Yamin",
+    c: "Chaerul Saleh",
+    d: "Soekarni",
+    e: "Johannes Latuharhary",
+    correct: "d",
+  },
+  {
+    question: "Pencipta lagu Indonesia Raya adalah?",
+    a: "Agus salim",
+    b: "Sutan Sjahrir",
+    c: "Wage Rudolf Supratman",
+    d: "Soeharto",
+    e: "Moh Hatta",
+    correct: "c",
+  },
+  {
+    question: "Siapa yang mengetik naskah proklamasi?",
+    a: "Dewi Sartika",
+    b: "Cut Nyak Dien",
+    c: "R.A. Kartini",
+    d: "Fatmawati",
+    e: "Sayuti Melik",
+    correct: "e",
+  },
+];
+
+const quizContainer = document.querySelector(".app");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.querySelector(".question");
+const aAnswer = document.getElementById("a--answer");
+const bAnswer = document.getElementById("b--answer");
+const cAnswer = document.getElementById("c--answer");
+const dAnswer = document.getElementById("d--answer");
+const eAnswer = document.getElementById("e--answer");
+const submit = document.querySelector(".btn");
+
+const AUDIOC = {
+  CLICK: new Audio('/assets/hooh.mp3'),
+}
+
+const AUDIOW = {
+  CLICK: new Audio('/assets/ente.mp3'),
+}
+
+let currentQuiz = 0;
+let score = 0;
+
+const resetAnswers = function () {
+  answerElements.forEach((el) => (el.checked = false));
+};
+
+const loadQuiz = function () {
+  resetAnswers();
+
+  const currentQuizData = quizData[currentQuiz];
+
+  questionElement.textContent = currentQuizData.question;
+  aAnswer.textContent = currentQuizData.a;
+  bAnswer.textContent = currentQuizData.b;
+  cAnswer.textContent = currentQuizData.c;
+  dAnswer.textContent = currentQuizData.d;
+  eAnswer.textContent = currentQuizData.e;
+};
+loadQuiz();
+
+const getSelected = function () {
+  let answer;
+  answerElements.forEach((el) => {
+    if (el.checked) answer = el.id;
+  });
+  return answer;
+};
+
+submit.addEventListener("click", function () {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      AUDIOC.CLICK.play()
+      score++;
+    } else {
+      AUDIOW.CLICK.play()
+    }
+
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quizContainer.innerHTML = `<h2 class="question">Kamu menjawab ${score}/${quizData.length} pertanyaan dengan benar</h2>
       <div class="d-grid gap-2 mt-5">
         <button class="btn btn-color-theme btn-lg" onclick="location.reload()" type="button">Main Lagi</button>
-      </div>`)
-})
+      </div>`;
+    }
+  }
+});
